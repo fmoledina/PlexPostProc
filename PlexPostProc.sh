479357f5574b78d3435c0e7afe518302fdb87201
fmoledina <faisal.moledina@gmail.com>
2018-01-28 22:39:45 -0700

    Included desired commits by benjroy and themoosman

--- PlexPostProc.sh
+++ PlexPostProc.sh
@@ -41,7 +41,7 @@ fatal() {
    echo "[FATAL] Program is now exiting.";
    exit 1;
 }
-# The above is a simple function for handeling fatal erros. (It outputs an error, and exits the program.)
+# The above is a simple function for handling fatal erros. (It outputs an error, and exits the program.)
 
 if [ ! -z "$1" ]; then 
 # The if selection statement proceeds to the script if $1 is not empty.
@@ -53,24 +53,35 @@ if [ ! -z "$1" ]; then
    FILENAME=$1 	# %FILE% - Filename of original file
 
    TEMPFILENAME="$(mktemp)"  # Temporary File for transcoding
+   
+   LOCKFILENAME="/tmp/PlexPostProcLock"
 
    # Uncomment if you want to adjust the bandwidth for this thread
    #MYPID=$$	# Process ID for current script
    # Adjust niceness of CPU priority for the current process
    #renice 19 $MYPID
+   
+   # Ensures no more than 1 convert process running at a time.
+   # Keeps CPU happy.
+   while [ -f "$LOCKFILENAME" ]; do sleep 10; done;
+   touch "$LOCKFILENAME"
 
    echo "********************************************************"
    echo "Transcoding, Converting to H.264 w/Handbrake"
    echo "********************************************************"
-   HandBrakeCLI -i "$FILENAME" -f mkv --aencoder copy -e qsv_h264 --x264-preset veryfast --x264-profile auto -q 16 --maxHeight 720 --decomb bob -o "$TEMPFILENAME" || fatal "Handbreak has failed (Is it installed?)"
-
+   #HandBrakeCLI -i "$FILENAME" -f mkv --aencoder copy -e qsv_h264 --x264-preset veryfast --x264-profile auto -q 16 --maxHeight 720 --decomb bob -o "$TEMPFILENAME" || fatal "Handbreak has failed (Is it installed?)"
+   HandBrakeCLI -i "$FILENAME" -o "$TEMPFILENAME" --format mkv --encoder x264 --quality 20 --loose-anamorphic --decomb veryfast --x264-preset fast --h264-profile high --h264-level 4.1  || fatal "Handbrake has failed (Is it installed?)"
+   
    echo "********************************************************"
    echo "Cleanup / Copy $TEMPFILENAME to $FILENAME"
    echo "********************************************************"
 
    rm -f "$FILENAME"
    mv -f "$TEMPFILENAME" "${FILENAME%.ts}.mkv"
-   chmod 777 "$FILENAME" # This step may no tbe neccessary, but hey why not.
+   chmod 775 "$FILENAME"
+   
+   # Let next conversion run.
+   rm -f "$LOCKFILENAME"
 
    echo "Done.  Congrats!"
 else
